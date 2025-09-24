4f8e5817879fc4576430c2270f969b9bafcd026a
Hala Yaghi <halayaghi@Halas-MacBook-Pro.local>
2025-09-24 14:15:40 +0200

    schema

---
+++ schemas.py
@@ -0,0 +1,17 @@
+# in this file i define the schemas (data validation)
+from pydantic import BaseModel
+
+class RecipeBase(BaseModel): # this is a class that is like the base of a recipe
+    title: str
+    ingredients: str
+    cuisine: str
+    meal_type: str
+    instructions: str
+
+class RecipeCreate(RecipeBase): # to create a new recipe, we refer to the base class
+    pass
+
+class Recipe(RecipeBase): # to read a recipe from the db, refer to base class schema and id
+    id: int
+    class Config:
+        orm_mode = True
