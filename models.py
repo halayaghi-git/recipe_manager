0d92513ef65f8288d966a6e0e9c41d43337f7d48
Hala Yaghi <halayaghi@Halas-MacBook-Pro.local>
2025-09-24 14:08:08 +0200

    db model

---
+++ models.py
@@ -0,0 +1,14 @@
+# in this file we define the database models using SQLAlchemy ORM
+# we have recipes.db as our database file
+
+from sqlalchemy import Column, Integer, String
+from database import Base
+
+class Recipe(Base): # this Recipe class represents the recipes table in the db
+    __tablename__ = "recipes"
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    ingredients = Column(String)
+    cuisine = Column(String, index=True)
+    meal_type = Column(String, index=True)
+    instructions = Column(String)
